0c61b6a8eb54aa23c7c1a378a28062e55bb61a0c
netra2127 <netra200021kcbdr@gmail.com>
2024-05-13 08:17:45 +0100

    merge sort

---
+++ Recursion/08_mergerSort.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+ * Mergesort. 
+ * Also called Divide and Merge. 
+ * Left-side -> (start, mid)
+ * Right-side -> (mid, end)
+ */
+
+void mergeSort(int arr[], int start, int end){
+    if(start == end){
+        return;
+    }
+    int mid = start + (end-start)/2;
+    mergeSort(arr, start, mid);
+    mergeSort(arr, mid+1, end);
+    merge(arr, start, mid, end);
+}
+
+void merge(int arr[], int start, int mid, int end){
+    vector<int> temp(end-start+1);
+    int left = start, right = mid + 1, index = 0;
+    while(left <= mid && right <= end){
+        if(arr[left]<=arr[right]){
+            temp[index] =  arr[left];
+            index++, left++;
+        }
+        else{
+            temp[index] =  arr[right];
+            index++, right++;
+        }
+    }
+    //left array elements. 
+    while(left<= mid){
+        temp[index] = arr[left];
+        left++, index++;
+    }
+
+    // right array elements. 
+    while(right<=end){
+        temp[index]=arr[right];
+        right++, index++;
+    }
+
+    index = 0;
+    while(start<=end){
+        arr[start] = temp[index];
+        start++, index++;
+    }
+}
+
+int main(){
+
+}
